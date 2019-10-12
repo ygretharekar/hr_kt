125b78d813b52929d47a55dcddf6353ad12a222c
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-10-13 04:29:09 +0530

    got 22.67 points

--- src/main/kotlin/medium/graph_theory/SynchronizedShopping.kt
+++ src/main/kotlin/medium/graph_theory/SynchronizedShopping.kt
@@ -1,8 +1,89 @@
 package medium.graph_theory
 
+import kotlin.math.max
+import kotlin.math.min
+
 class SynchronizedShopping {
+
+    private var shops = mutableListOf<Int>()
+    private var shortestPath: MutableMap<Int, MutableList<Int>> = mutableMapOf()
+    private var queue: MutableSet<Pair<Int, Pair<Int, Int>>> = mutableSetOf<Pair<Int, Pair<Int, Int>>>()
+
+    private fun inQueue(node: Int, fishes: Int, cost: Int, fishNum: Int): Unit {
+        if (shortestPath[node]?.getOrNull(fishes) ?: Int.MAX_VALUE <= cost) {
+            return
+        }
+
+        queue.remove(shortestPath[node]?.getOrNull(fishes) ?: Int.MAX_VALUE to (node to fishes))
+        val pathList: MutableList<Int> =
+            shortestPath.getOrPut<Int, MutableList<Int>>(node) { MutableList(1 shl fishNum) { Int.MAX_VALUE } }
+        pathList[fishes] = cost
+
+        queue.add(cost to (node to fishes))
+    }
+
+    private fun dijkdtra(src: Int, fishNum: Int, biRoads: Array<MutableList<Pair<Int, Int>>>) {
+        inQueue(src, shops[src], 0, fishNum)
+
+        while (queue.isNotEmpty()) {
+            val cost = queue.first().first
+            val currentFishes = queue.first().second.second
+            val node = queue.first().second.first
+
+            queue.remove(queue.first())
+
+            for (road in biRoads[node]) {
+                inQueue(road.first, shops[road.first] or currentFishes, cost + road.second, fishNum)
+            }
+        }
+    }
+
     fun shop(n: Int, k: Int, centers: Array<String>, roads: Array<Array<Int>>): Int {
         // Write your code here
-        return 0
+
+        val fishShops = centers.map { s ->
+            s
+                .split(" ")
+                .map {
+                    it.toInt()
+                }
+        }
+
+        val biRoads: Array<MutableList<Pair<Int, Int>>> = Array(n) { mutableListOf<Pair<Int, Int>>() }
+
+        roads
+            .forEach {
+                biRoads[it[0] - 1].add(Pair(it[1] - 1, it[2]))
+                biRoads[it[1] - 1].add(Pair(it[0] - 1, it[2]))
+            }
+
+        fishShops
+            .forEach { a ->
+                var fishes: Int = 0
+                a
+                    .drop(1)
+                    .forEach {
+                        fishes = fishes or (1 shl it - 1)
+                    }
+
+                shops.add(fishes)
+            }
+
+        dijkdtra(0, k, biRoads)
+
+        var ans = Int.MAX_VALUE
+
+        shortestPath[n - 1]?.forEachIndexed { i, i1 ->
+            shortestPath[n - 1]?.forEachIndexed { j, j1 ->
+                if (i1 != Int.MAX_VALUE && j1 != Int.MAX_VALUE && (i or j) == (1 shl k) - 1) {
+                    ans = min(
+                        ans,
+                        max(i1, j1)
+                    )
+                }
+            }
+        }
+
+        return ans
     }
 }
--- src/test/kotlin/medium/graph_theory/SynchronizedShoppingTest.kt
+++ src/test/kotlin/medium/graph_theory/SynchronizedShoppingTest.kt
@@ -6,10 +6,23 @@ import org.junit.jupiter.api.Assertions.*
 import medium.graph_theory.SynchronizedShopping
 
 internal class SynchronizedShoppingTest {
-
     @Test
     fun shop() {
         val synchronizedShopping: SynchronizedShopping = SynchronizedShopping()
-        assertEquals(0, synchronizedShopping.shop(0, 0, arrayOf("abc", "abc"), Array(1)  { arrayOf(1, 2, 3)}))
+        assertEquals(
+            1,
+            synchronizedShopping.shop(
+                5,
+                5,
+                arrayOf("1 1", "1 2", "1 3", "1 4", "1 5"),
+                arrayOf(
+                    arrayOf(1, 2, 10),
+                    arrayOf(1, 3, 10),
+                    arrayOf(2, 4, 10),
+                    arrayOf(3, 5, 10),
+                    arrayOf(4, 5, 10)
+                )
+            )
+        )
     }
 }
